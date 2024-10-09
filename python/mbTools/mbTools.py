810da7078328448cae14af2aa5ace87ae5045175
Melodie Borel <melodieborel@syntuitio.com>
2024-10-09 14:38:58 +0200

    improve superCleanPlot

--- python/mbTools/mbTools.py
+++ python/mbTools/mbTools.py
@@ -27,30 +27,48 @@ class color:
    UNDERLINE = '\033[4m'
    END = '\033[0m'
 
-def superCleanPlot(obj1, obj2, time=0, pre=1, post=4):
-    import matplotlib.pyplot as plt
-    plt.close()
+def superCleanPlot(lfp, npx, canauxLFP=None, structureLFP=None, canauxNPX=[0,1], time=0, pre=1, post=4, offset=0, scaleNPX=1, scaleLFP=1):
+   """
+   superCleanPlot plots very precisely aligned NPX and LFP data
 
-    #offset=51.51#51.4576900#t_start['LFP']#52.6734#52.68
-    # perfect align manual offset=51.5146156977
-    #offset=51.52262754
+   :lfp: the lfp object containing signal, times, sampling_rate...
+   :npx: the npx object containing all infos as well
+   :canauxLFP: (facultative, None by default) array of int that indicates the channels to display. StructureLFP will be ignored if canauxLFP is not None
+   :structureLFP: (facultative, None by default) array of strings that indicates the brain structures defined by mapping to display. Will be ignored if canauxLFP is not None
+   :canauxNPX: ((facultative, default is [0,1]) array of ints that indicates the channels to display. For ann interval, please enter np.arange(start, stop).
+   :time: the time to center on display in seconds
+   :pre: duration (float in s) before time of interest to display 
+   :post: duration (float in s) before time of interest to display 
+   """
+   import matplotlib.pyplot as plt
+   plt.close()
 
-    idx=find_nearest(obj1.times, time)
-    print(obj1.times[idx])
-    print(idx)
+   #offset=51.51#51.4576900#t_start['LFP']#52.6734#52.68
+   # perfect align manual offset=51.5146156977
+   #offset=51.52262754
 
-    x=obj1.times[idx-int(pre*obj1.sampling_rate):idx+int(post*obj1.sampling_rate)]
-    y=obj1.signal[idx-int(pre*obj1.sampling_rate):idx+int(post*obj1.sampling_rate),:]
+   idx=find_nearest(lfp.times, time)
+   print(lfp.times[idx])
+   print(idx)
 
-    print(obj2.times)
-    idx2=find_nearest(obj2.times, time)
-    print(idx2)
-    x2=obj2.times[idx2-int(pre*obj2.sampling_rate):idx2+int(post*obj2.sampling_rate)]
-    y2=obj2.signal['spike'].get_traces(start_frame=idx2-int(pre*obj2.sampling_rate), end_frame=idx2+int(post*obj2.sampling_rate), return_scaled=False)
+   x=lfp.times[idx-int(pre*lfp.sampling_rate):idx+int(post*lfp.sampling_rate)]
+   if canauxLFP is not None:
+      y=lfp.signal[idx-int(pre*lfp.sampling_rate):idx+int(post*lfp.sampling_rate),canauxLFP]
+   elif structureLFP is not None:
+      y=lfp.combineStructures(structureLFP)[idx-int(pre*lfp.sampling_rate):idx+int(post*lfp.sampling_rate),:]
+   else:
+      y=lfp.signal[idx-int(pre*lfp.sampling_rate):idx+int(post*lfp.sampling_rate),:]
 
-    plt.plot(x, y,'-')
-    plt.plot(x2, np.transpose(y2[:,0])*10,'-')
-    plt.show()
+
+   print(npx.times)
+   idx2=find_nearest(npx.times, time)
+   print(idx2)
+   x2=npx.times[idx2-int(pre*npx.sampling_rate):idx2+int(post*npx.sampling_rate)]
+   y2=npx.signal['spike'].get_traces(start_frame=idx2-int(pre*npx.sampling_rate), end_frame=idx2+int(post*npx.sampling_rate), return_scaled=False)
+
+   plt.plot(x, y*scaleLFP,'-')
+   plt.plot(x2, y2[:,canauxNPX]*scaleNPX+offset,'-')
+   plt.show()
 
 def convertTheoricIndex2realTime(thIdx,realFreq=1, offset=0):
     realTime=thIdx/realFreq + offset
