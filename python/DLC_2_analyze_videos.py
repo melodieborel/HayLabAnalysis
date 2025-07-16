26da0d5765f4c7f249cfe1ca242ad5f5deb4f3b7
rubaray <157726753+r19847@users.noreply.github.com>
2025-07-16 17:55:12 +0200

    FED

---
+++ python/DLC_2_analyze_videos.py
@@ -0,0 +1,17 @@
+##### Called by autoProcessing_dlc.sh #####
+
+import deeplabcut
+import time
+
+st = time.time()
+
+path_config_file="C:/Users/AudreyHay/Documents/Carla/DeepLabCut/Cheeseboard-CBM-2025-02-06/config.yaml"
+videofile_path = ["C:/Users/AudreyHay/Documents/Carla/DeepLabCut/Cheeseboard-CBM-2025-02-06/videos"] #will analyse all .avi files in this directory
+
+deeplabcut.analyze_videos(path_config_file,videofile_path, videotype='.mp4')#, save_as_csv=True)
+#deeplabcut.filterpredictions(path_config_file, videofile_path, shuffle=1, p_bound=0.01, ARdegree=3, MAdegree=1)
+#deeplabcut.create_labeled_video(path_config_file,videofile_path)
+#deeplabcut.plot_trajectories(path_config_file,videofile_path, imagetype=".svg")
+
+elapsed_time = time.time() - st
+print('Execution time:', time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
--- python/DLC_train_network.py
+++
@@ -1,8 +0,0 @@
-import deeplabcut
-
-path_config_file="/mnt/data/AurelieB_dlc/CheeseboardMiniscope-AurelieB-2025-02-21/config.yaml"
-
-#deeplabcut.create_training_dataset(path_config_file,Shuffles=[1])
-#deeplabcut.train_network(path_config_file, shuffle=1, saveiters=1000, displayiters=10)
-
-deeplabcut.evaluate_network(path_config_file, plotting=True)
