51e3834bffcfb4a4b1b47114f4c824fc63d33c21
Aurélie Brécier <156210381+AurelieBre@users.noreply.github.com>
2025-08-26 17:24:02 +0200

    Update h5 analysis

--- python/DLC_2_analyze_videos.py
+++ python/DLC_2_analyze_videos.py
@@ -7,6 +7,7 @@ st = time.time()
 
 path_config_file='/home/aurelie.brecier/CheeseboardMiniscope-AurelieB-2025-02-21/config.yaml'
 videofile_path = ["/mnt/data/AurelieB_dlc/"] #will analyse all .avi files in this directory
+#videofile_path = ["//10.69.168.1/crnldata/forgetting/Aurelie/MiniscopeOE_data/"] #will analyse all .avi files in this directory
 
 deeplabcut.analyze_videos(path_config_file,videofile_path, videotype='.avi')#, save_as_csv=True)
 #deeplabcut.filterpredictions(path_config_file, videofile_path, shuffle=1, p_bound=0.01, ARdegree=3, MAdegree=1)
