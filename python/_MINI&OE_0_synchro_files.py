ca09073be2eac029b0b4fd98d0773367328c4da2
Aurélie Brécier <156210381+AurelieBre@users.noreply.github.com>
2025-10-13 16:27:20 +0200

    Allow for TTL synchronsation (with Lyon setup)

--- python/_MINI&OE_0_synchro_files.py
+++ python/_MINI&OE_0_synchro_files.py
@@ -14,11 +14,11 @@ import xarray as xr
         # DEFINE PATH #
 ##################################
 
-local = False
+local = True
 if local:
-    DIR= Path("//10.69.168.1/crnldata/forgetting/Aurelie/MiniscopeOE_data/L2_3_mice/BC/PlaceCells_experiment/")
+    DIR= Path("//10.69.168.1/crnldata/forgetting/Aurelie/MiniscopeOE_data/L2_3_mice/")
 else: 
-    DIR= Path("/crnldata/forgetting/Aurelie/MiniscopeOE_data/L2_3_mice/BC/PlaceCells_experiment/")
+    DIR= Path("/crnldata/forgetting/Aurelie/MiniscopeOE_data/L2_3_mice/")
 
 ##################################
     # CREATE SYNCHRO FILE #
@@ -27,113 +27,96 @@ else:
 for dpath in DIR.glob('**/*V4_Miniscope*/'):
 
     folder = Path(dpath.parent)
-    file = next(folder.rglob('SynchroFileCorrect.xlsx'), None)
+    SynchroFile = next(folder.rglob('SynchroFile.xlsx'), None)
 
-    if not file: # process only if there are no SynchroFile already 
+    if not SynchroFile: # process only if there are no SynchroFile already 
         
         try: # in case no sync file
 
-            print("####################################################################################################################################################")
-            print(folder)
-            print("####################################################################################################################################################")
+            if any(folder.parent.rglob('OpenEphys/*')):
 
-            for dir_path in folder.glob('**/*V4_Miniscope*/'):
-                file_path= f'{dir_path}/timeStamps.csv'
-                stamps_miniscope = pd.read_csv(file_path)
+                print("####################################################################################################################################################")
+                print(folder)
+                print("####################################################################################################################################################")
 
-            for file_path in folder.glob('**/LFP_timeStamps.npy'):
-                subfolder = file_path.parents[0].stem
-                file = file_path.stem
-                stamps_OEmini = np.load(file_path)
-                datalen = len(stamps_OEmini)
-                coords = {
-                    'recordings' : np.array(['full_words', 'timestamps', 'channel_states', 'channels']),
-                    'duration_rec' : np.arange(datalen)
-                }
-                Allstamps = xr.DataArray(coords=coords, dims=['recordings', 'duration_rec'])
-                Allstamps.loc[file,:] = stamps_OEmini                    
+                for dir_path in folder.glob('**/*V4_Miniscope*/'):
+                    file_path= f'{dir_path}/timeStamps.csv'
+                    stamps_miniscope = pd.read_csv(file_path)
 
-            time = range(datalen)
-            fullwords = Allstamps.loc['full_words',:].values
-            timestamps = Allstamps.loc['timestamps',:].values
-            channelstates = Allstamps.loc['channel_states',:]
-            channels = Allstamps.loc['channels',:]
+                recordings = ['TTL_full_words', 'TTL_sample_numbers', 'TTL_states', 'TTL_timestamps']
+                Allstamps = {}
+                for file_path in folder.parent.glob('**/TTL*.npy'):
+                    filename = file_path.stem
+                    print(f"Loading {filename}")
+                    Allstamps[filename] = np.load(file_path)
 
-            stamps_miniscope_time = stamps_miniscope['Time Stamp (ms)']
-            delay_stamps = []
-            number_frames = stamps_miniscope['Time Stamp (ms)'].count()
-            for i in range(number_frames -1):
-                delay_stamps.append(stamps_miniscope_time[i+1] - stamps_miniscope_time[i])
+                datalen = max(len(arr) for arr in Allstamps.values())
+                fullwords      = Allstamps.get('TTL_full_words', np.full(datalen, np.nan))
+                timestamps     = Allstamps.get('TTL_timestamps', np.full(datalen, np.nan))
+                channelstates  = Allstamps.get('TTL_states', np.full(datalen, np.nan))
+                channels       = Allstamps.get('TTL_sample_numbers', np.full(datalen, np.nan))
 
 
-            freq_acq = round(1000/(sum(delay_stamps)/len(delay_stamps)))
-            print("The calculated frame rate is : {} Hz".format(freq_acq) )
-
-            delaystampMini = (1000/freq_acq)*1.5
-            print('Threshold dropped frame=',delaystampMini)
-
-            delay_stamps = []
-            dropped_frames = []
-            number_frames = stamps_miniscope['Time Stamp (ms)'].count()
-            for i in range(number_frames -1):
-                delay_stamps.append(stamps_miniscope_time[i+1] - stamps_miniscope_time[i])
-                if delay_stamps[i] > delaystampMini:
-                    dropped_frames.append(i)
-
-            print("{} frame(s) were dropped : {}".format(len(dropped_frames),dropped_frames))
+                stamps_miniscope_time = stamps_miniscope['Time Stamp (ms)']
+                delay_stamps = []
+                number_frames = stamps_miniscope['Time Stamp (ms)'].count()
+                for i in range(number_frames -1):
+                    delay_stamps.append(stamps_miniscope_time[i+1] - stamps_miniscope_time[i])
 
 
-            OE_stamps_miniscope = []
-            OE_stamps_laser = []
-            for i in range(datalen):
-                if channels[i] == 2:
-                    OE_stamps_miniscope.append(timestamps[i])
-                elif channels[i] == 1:
-                    OE_stamps_laser.append(timestamps[i])
+                freq_acq = round(1000/(sum(delay_stamps)/len(delay_stamps)))
+                print("The calculated frame rate is : {} Hz".format(freq_acq) )
+
+                delaystampMini = (1000/freq_acq)*1.5
+                print('Threshold dropped frame=',delaystampMini)
+
+                delay_stamps = []
+                dropped_frames = []
+                number_frames = stamps_miniscope['Time Stamp (ms)'].count()
+                for i in range(number_frames -1):
+                    delay_stamps.append(stamps_miniscope_time[i+1] - stamps_miniscope_time[i])
+                    if delay_stamps[i] > delaystampMini:
+                        dropped_frames.append(i)
+
+                print("{} frame(s) were dropped : {}".format(len(dropped_frames),dropped_frames))
 
 
-            A = []
-            for file_path in folder.glob('**/sync_messages.txt'):
-                with open(file_path, "r") as f:
-                    lines = f.readlines()
-                # Loop through all lines, ignoring header.
-                    for l in lines[1:]:
-                        A.append(l.split()[-1])# take last element to list (i.e. the process name)
-
-            # remove acquisition frequency that is normally always 25 kHz
-            initial_OE_start = int(' '.join([x.split('@')[0] for x in A]))
-            acqFreqOE = int(' '.join([x[:-2].split('@')[1] for x in A]))
-
-            # transform in pd series for easier manipulation
-            B = pd.Series(OE_stamps_miniscope)
-            C = pd.Series(OE_stamps_laser)
-
-            # normalise to ms
-            OE_stamps_miniscope_n = (B - initial_OE_start)/acqFreqOE
-            OE_stamps_laser_inter = (C - initial_OE_start)/acqFreqOE
-
-            # take only the middle of the laser pulse
-            OE_stamps_laser_n = [] 
-            for i in range(len(OE_stamps_laser_n) - 1):
-                if (OE_stamps_laser_n[i+1] - OE_stamps_laser_n[i]) < freq_acq:
-                    interm = OE_stamps_laser_n[i] + 10
-                    OE_stamps_laser_n.append(interm)
+                OE_stamps_miniscope = []
+                for i in range(datalen):
+                    if channelstates[i] == 8: # TTL 8 is Miniscope
+                        OE_stamps_miniscope.append(timestamps[i])
 
 
-            # _n is from 0
-            acquisition_mini_start_n = OE_stamps_miniscope_n[0]
-            # _a is from acquisition time software
-            acquisition_mini_start_a = B[0]
+                for file_path in folder.parent.glob('**/sync_messages.txt'):
+                    with open(file_path, "r") as f:
+                        lines = f.readlines()
+                    # Loop through all lines, ignoring header.
+                        for l in lines[1:]:
+                            initial_OE_start = int(l.split()[-1] )
+                            acqFreqOE = int(l.split()[-3] )
 
-            outSumm = pd.Series([acquisition_mini_start_n, acquisition_mini_start_a, freq_acq, dropped_frames],
-                        index=['Miniscope start from 0', 'Miniscope start from Acq time soft', 'mini acq freq', 'dropped frames'])
-            print(outSumm)
+                # transform in pd series for easier manipulation
+                B = pd.Series(OE_stamps_miniscope)
 
-            filenameOut = folder / f'SynchroFileCorrect.xlsx'
-            writer = pd.ExcelWriter(filenameOut)
-            outSumm.to_excel(writer)
-            writer.close()
+                # normalise to ms
+                OE_stamps_miniscope_n = B - (initial_OE_start/acqFreqOE)
 
+                # _n is from 0
+                acquisition_mini_start_n = OE_stamps_miniscope_n[0]
+                # _a is from acquisition time software
+                acquisition_mini_start_a = B[0]
+
+                outSumm = pd.Series([acquisition_mini_start_n, acquisition_mini_start_a, freq_acq, dropped_frames],
+                            index=['Miniscope start from 0', 'Miniscope start from Acq time soft', 'mini acq freq', 'dropped frames'])
+                print(outSumm)
+
+                filenameOut = folder / f'SynchroFile.xlsx'
+                writer = pd.ExcelWriter(filenameOut)
+                outSumm.to_excel(writer)
+                writer.close()
+            else:
+                print('NO OPENEPHYS FOLDER. Skipping...')
+                continue
         except:
             print('NO SYNC FILE SAVED. Skipping...')
             continue
